4105b590add75e7522c9c6f19a6a022d987742c6
allthatilk <imogenkutz@hotmail.com>
2017-03-16 10:22:36 +0000

    WIP errors working and tested properly

--- spec/airportSpec.js
+++ spec/airportSpec.js
@@ -1,23 +1,24 @@
 describe("Airport", function() {
 
   beforeEach(function () {
-    plane = new Plane();
-    airport = new Airport();
-});
+    plane = new Plane()
+    airport = new Airport()
+  })
 
   describe("status", function() {
     it("contains plane", function() {
-      airport.landingPermission(plane);
-      expect(airport.runways.indexOf(plane)).toEqual(0);
-    });
+      airport.landingPermission(plane)
+      expect(airport.runways.indexOf(plane)).toEqual(0)
+    })
+
     it("has a maximum capacity", function() {
-      for(var i = 0; i < 9; i++){
-        jet = new Plane;
-        airport.landingPermission(jet);
+      for (let i = 0; i < 10; i++) {
+        airport.landingPermission(new Plane())
       }
-      boeing = new Plane();
-      expect(airport.landingPermission(boeing)).toEqual(console.log("Airport is full"));
-    }
-  )
-  });
-});
+
+      const boeing = new Plane()
+      expect(airport.landingPermission(boeing)).
+        toEqual('Airport is full')
+    })
+  })
+})
--- src/airport.js
+++ src/airport.js
@@ -4,7 +4,7 @@ function Airport() {
 
 Airport.prototype.landingPermission = function(plane) {
     if (this.runways.length === 10)
-      console.log("Airport is full");
+      return "Airport is full"
     else {
       plane.land();
       this.runways.push(plane);
